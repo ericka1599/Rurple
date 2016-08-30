7e00447c6ba1b6278630321dc9d180145f77f9b4
ericka1599 <erickasegura96@gmail.com>
2016-08-30 08:45:35 -0600

    :)

--- mapa.py
+++ mapa.py
@@ -1,52 +1,36 @@
-from rurple import cargar_mapa
 class Mapa ():
 	def __init__ (self, ancho, alto):
 		self.largo = largo
 		self.ancho = ancho
 		self.monedas = []
 		slef.robot = None
+
+
+	def asignar_robot (self, robot):
+		self.robot = robot
 	
-	def medir_ancho (self):
-		ancho = len(lista_mapa[0,0])
-		self.ancho = ancho
+	def contar_monedas (self, moneda):
+		self.monedas.append(moneda)
 
-	def medir_alto (self):
-		alto = len(lista_mapa/ancho)
-		self.largo = largo
+	def dibujar_mapa (self):
+		resultado = ""
+		for y in range(self.altura):
+			for x in range(self.ancho):
+				if monedas.x == x and monedas.y == y:
+					resultado = mone
 
-	def encontrar_monedas (self):
-		moneda_encontrada = False 
-		monedas_encontradas = 0
-		for i in lista_mapa:
-			if i != 0:
-				moneda_encontrada = True
-				monedas_encontradas += 1
-				if moneda_encontrada == True:
-					return i
+	def crear (self, mapa, monedas, robot):
+		mapa = Mapa (len(lista_mapa[0])) , len(lista_mapa)
+		for y in range(len(lista_mapa)):
+			for x in range(len(lista_mapa[y])):
+				if lista_mapa [x][y] == "*":
+					robot = Robot (x, y)
+					mapa.poner_robot(Robot)
+				elif lista_mapa [x][y] == "0":
+					return " "
+				else:
+					cantidad = int(lista_mapa[y][x])
+					for i in range(cantidad):
+						moneda = Moneda(x, y)
+						mapa.agregar_moneda(Moneda)
 
-	def mostrar_mapa (self):
-
-
-
-
-
-
-
-
-
-
-
-def crear (mapa, monedas, robot):
-	mapa = Mapa (len(lista_mapa[0])) , len(lista_mapa)
-	for y in range(len(lista_mapa)):
-		for x in range(len(lista_mapa[y])):
-			if lista_mapa [x][y] == "*":
-				robot = Robot (x, y)
-				mapa.poner_robot(Robot)
-			elif lista_mapa [x][y] == "0":
-				return " "
-			else:
-				cantidad = int(lista_mapa[y][x])
-				for i in range(cantidad):
-					moneda = Moneda(x, y)
-					mapa.agregar_moneda(Moneda)
--- robot.py
+++ robot.py
@@ -1,37 +1,27 @@
-from mapa import medir_alto
-from mapa import medir_ancho 
-import time 
 class Robot(object):
 	def __init__ (self, x, y):
 		self.x = medir_ancho
 		self.y = medir_alto
 		self.monedas = 0
-		self.direccion = 'UP'
+		self.direccion = 0
 		self.mapa = None 
 
 	def colocar_en_mapa (self, mapa):
 		self.mapa = mapa
 
-	def posicion (x, y):
-		resultado = (" " * ancho) * y
-		resultado += " " * x + "*"
-		resultado += " " + ancho * (alto - (y + 1))
-		return resultado
+	def  rotar (self):
+		if self.direccion = (self.direccion + 1) % 4
 
 
-#Aqui deberia ser con las instrucciones
-		steps = 19 
-		for i in range (steps):
-			time.sleep(0.2)
-			print(posicion(5 + i, i)) 
-
-	def mostrar_robot (self, direccion):
-		if self.direccion == 'UP':
+	def dibujar (self):
+		if self.direccion == 0:
 			return '↑'
-		elif self.direccion == 'RIGHT':
+		elif self.direccion == 1:
 			return '→'
-		elif self.direccion == 'LEFT':
+		elif self.direccion == 2:
 			return '←'
 		else:
 			return '↓'
+
+	def recoger_monedas (self, x, y):
 
